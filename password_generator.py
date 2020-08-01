b042ca7f16447983706d0c37d58643a2fcba480c
Jaspreet Singh <44704071+singh-jp-7@users.noreply.github.com>
2020-08-01 12:18:58 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+import random
+password = ""
+characters = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! @ # $ % & * ? ] [ { }"
+char_list = characters.split()
+n = int(input("Enter the password length: "))
+passwd = random.sample(char_list, n)
+password = password.join(passwd)
+print(password)
